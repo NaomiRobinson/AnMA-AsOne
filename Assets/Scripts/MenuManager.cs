2710efce6747778976708917f0339deb671b1716
Bianca Ercole <127907362+biancuercole@users.noreply.github.com>
2025-05-24 09:46:57 -0300

    error arreglado

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -20,6 +20,18 @@ public class MenuManager : MonoBehaviour
     public void ReturnToMenu()
     {
         SceneManager.LoadScene("Menu");
+
+        // Reiniciar contadores de muerte y tiempo
+        if (GameStats.Instance != null)
+        {
+            GameStats.Instance.tiempoTotal = 0f;
+            GameStats.Instance.muertesTotales = 0;
+        }
+
+        // También podés reiniciar los contadores de StaticVariables si querés
+        StaticVariables.SessionData.death = 0;
+        StaticVariables.SessionData.time = 0;
+        StaticVariables.SessionData.level = 1;
     }
 
 
